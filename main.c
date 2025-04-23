5c09318bf20b958689ad18166d63f4b30d65e02c
Eduardo Henrique Moraes Gama <tr26357es@trf1.gov.br>
2025-04-23 15:35:16 -0300

    Formatando os prints e mudando a ordem das funçôes vetorAleatorio e medidaDeTempoFuncao

--- main.c
+++ main.c
@@ -1,51 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
-
-void vetorAleatorio(int arr[], int n) {
-  for(int i = 0; i < n; i++)
-      arr[i] = rand() % 20000;
-}
-
-void medidaDeTempoFuncao(const char *funcao, int *vetor, int n)
-{
-  int i;
-
-  int *vetor_tmp = malloc(n * sizeof(int));
-  memcpy(vetor_tmp, vetor, n * sizeof(int));
-
-  clock_t inicio = clock();
-
-  if (strcmp(funcao, "BubbleSort") == 0)
-  {
-    bubbleSort(vetor_tmp, n);
-    printf("\n\nVetor ordenado com BubbleSort\n");
-  }
-  if (strcmp(funcao, "QuickSort") == 0)
-  {
-    quickSort(vetor_tmp, 0, n-1);
-    printf("\n\nVetor ordenado com QuickSort\n");
-  }
-  if (strcmp(funcao, "MergeSort") == 0)
-  {
-    mergeSort(vetor_tmp, n);
-    printf("\n\nVetor ordenado com MergeSort\n");
-  }
-
-  clock_t fim = clock();
-  double elapsed_ms = (double)(fim - inicio) * 1000.0 / CLOCKS_PER_SEC;
-  printf("%s: Tempo decorrido: %8.3f ms\n", funcao, elapsed_ms);
-
-  printf("\nVetor depois da ordenação (10 primeiros elementos): \n");
-  for (int i = 0; i < 10; i++)
-  {
-    printf("[%d] ", vetor_tmp[i]);
-  }
-}
 
 void troca(int *vetor, int valor1, int valor2)
 {
-
   int auxiliar = vetor[valor1];
   vetor[valor1] = vetor[valor2];
   vetor[valor2] = auxiliar;
@@ -77,7 +36,6 @@ int particionar(int *vetor, int inicio, int fim)
 
 int particionamentoAleatorio(int *vetor, int inicio, int fim)
 {
-
   int pivoIndice = (rand() % (fim - inicio + 1)) + inicio;
 
   troca(vetor, pivoIndice, fim);
@@ -154,14 +112,54 @@ void mergeSort(int *vetor, int tamanho)
   }
 }
 
+void vetorAleatorio(int arr[], int n) {
+  for(int i = 0; i < n; i++)
+    arr[i] = rand() % 20000;
+}
+
+void medidaDeTempoFuncao(const char *funcao, int *vetor, int n)
+{
+  int i;
+
+  int *vetor_tmp = malloc(n * sizeof(int));
+  memcpy(vetor_tmp, vetor, n * sizeof(int));
+
+  clock_t inicio = clock();
+
+  if (strcmp(funcao, "BubbleSort") == 0)
+  {
+    bubbleSort(vetor_tmp, n);
+  }
+  if (strcmp(funcao, "QuickSort") == 0)
+  {
+    quickSort(vetor_tmp, 0, n-1);
+  }
+  if (strcmp(funcao, "MergeSort") == 0)
+  {
+    mergeSort(vetor_tmp, n);
+  }
+  
+  clock_t fim = clock();
+  double elapsed_ms = (double)(fim - inicio) * 1000.0 / CLOCKS_PER_SEC;
+
+  printf("\n\nVetor ordenado com %s - Tempo Decorrido: %8.3f ms\n", funcao, elapsed_ms);
+  printf("Vetor depois da ordenação (10 primeiros elementos): \n");
+  for (int i = 0; i < 10; i++)
+  {
+    printf("[%d] ", vetor_tmp[i]);
+  }
+}
+
+
 int main()
 {
   srand((unsigned) time(NULL));
+
   int vetor[10] = {1, 3, 2, 8, 3, 4, 6, 5, 9, 10};
-  int size_vetor = sizeof(vetor);
   int i = 0;
 
-  printf("\nVetor antes da ordenação\n");
+  printf("\n=== Vetor inicial de 10 elementos ===\n");
+  printf("\nVetor antes da ordenação:\n");
   for (int i = 0; i < 10; i++)
   {
     printf("[%d] ", vetor[i]);
@@ -176,10 +174,16 @@ int main()
 
   for (int i = 0; i < num_sizes; i++) {
     int n = sizes[i];
-    printf("\n=== Vetor de %d elementos ===\n", n);
+    printf("\n\n=== Vetor de %d elementos ===\n", n);
 
     int *vetor_random = malloc(n * sizeof(int));
     vetorAleatorio(vetor_random, n);
+
+    printf("\nVetor antes da ordenação (10 primeiros elementos)\n");
+    for (int i = 0; i < 10; i++)
+    {
+      printf("[%d] ", vetor_random[i]);
+    }
 
     medidaDeTempoFuncao("BubbleSort", vetor_random, n);
     medidaDeTempoFuncao("QuickSort",  vetor_random, n);
